cab443ba2d8e8da41ce9458cc875e0ad4acc13eb
ehabterra <ehabterra@hotmail.com>
2020-04-20 19:26:47 +0000

    Added hostname

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 // src/server.ts
 import "./utils/config"
 import app from "./app";
-const PORT = process.env.PORT || 3100;
+const PORT: number = parseInt(process.env.PORT || '3100');
+const HOSTNAME = process.env.HOSTNAME || 'localhost';
 
-console.log(`Database: ${process.env.Database}`);
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
+// console.log(`Database: ${process.env.Database}`);
+app.listen(PORT, HOSTNAME, () => console.log(`Example app listening on port ${HOSTNAME}:${PORT}!`));
